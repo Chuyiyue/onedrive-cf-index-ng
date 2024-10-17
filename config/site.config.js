db75fe518849512e1586bcf466f62e1a52fabbbe
ChuYiyue <92413270+Chuyiyue@users.noreply.github.com>
2024-10-17 21:49:45 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -48,11 +48,11 @@ module.exports = {
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   // links: [
-    {
-      name: 'GitHub',
-      link: 'https://github.com/lyc8503',
-    },
-  ],
+  //  {
+  //    name: 'GitHub',
+  //    link: 'https://github.com/lyc8503',
+  //  },
+  // ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
